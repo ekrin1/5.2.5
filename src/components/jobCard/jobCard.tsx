3434ccf61c6835fb97ef3291ca773ec7d2609a16
ekrin1 <kottovakatya@mail.ru>
2026-01-07 15:24:11 +0300

    вынесла функции из card

--- src/components/jobCard/jobCard.tsx
+++ src/components/jobCard/jobCard.tsx
@@ -1,6 +1,7 @@
 import type { JobType } from "../../services/types";
 import { Group, Button, Text, Badge } from "@mantine/core";
 import clsx from "clsx";
+import { formatSalary, formatExperience, getWorkFormat } from "../../utils/workFormat";
 
 import styles from "./jobCard.module.css";
 
@@ -9,88 +10,7 @@ type Props = {
 };
 
 export const JobCard = ({ vacancy }: Props) => {
-
-    const formatNumber = (num?: number) => {
-      if (!num) return "";
-      return new Intl.NumberFormat("ru-RU").format(num);
-    };
-
-    const formatCurrency = (currencyName?: string) => {
-      if (currencyName?.includes("RUR")) return "₽";
-      if (currencyName?.includes("KZT")) return "₸";
-      if (currencyName?.includes("USD")) return "$";
-      return currencyName;
-    };
-
-    const salary = vacancy.salary
-    ? `${vacancy.salary.from ? `${formatNumber(vacancy.salary.from)}` : ""} ${
-        vacancy.salary.to ? `– ${formatNumber(vacancy.salary.to)}` : ""
-        } ${formatCurrency(vacancy.salary.currency) || ""}`
-    : "Не указана";
-
-    const formatExperience = (experienceName?: string) => {
-      if (!experienceName) return "Не указано";
-
-    const normalized = experienceName.toLowerCase().trim();
-
-    if (normalized.includes("нет опыта")) {
-      return "Без опыта";
-    }
-
-    if (/от\s*\d+.*до\s*\d+/i.test(normalized)) {
-      const match = normalized.match(/от\s*(\d+).+до\s*(\d+)/i);
-      if (match) {
-        const from = Number(match[1]);
-        const to = Number(match[2]);
-        const yearsWord = to > 5 ? "лет" : "года";
-        return `Опыт ${from}–${to} ${yearsWord}`;
-      }
-    }
-
-      return experienceName;
-    };
-
-    const getWorkFormatData = (formatId: string) => {
-      switch (formatId) {
-
-      case "REMOTE":
-        return {
-          label: "Можно удалённо",
-          bg: "var(--mantine-color-primary-4)",
-          color: "var(--mantine-color-white-0)",
-        };
-
-      case "ON_SITE":
-        return {
-          label: "Офис",
-          bg: "var(--mantine-color-ultraLight-9)",
-          color: "var(--mantine-color-gray-9)",
-        };
-
-      case "HYBRID":
-        return {
-          label: "Гибрид",
-          bg: "var(--mantine-color-black-9)",
-          color: "var(--mantine-color-white-0)",
-        };
-
-      default:
-        return {
-          label: "Не указано",
-          bg: "var(--mantine-color-gray-1)",
-          color: "var(--mantine-color-black-9)",
-        };
-    }
-  };
-
-    const workFormat = vacancy.work_format?.[0];
-    const { label, bg, color } = workFormat
-      ? getWorkFormatData(workFormat.id)
-      : {
-          label: "Не указано",
-          bg: "var(--mantine-color-gray-1)",
-          color: "var(--mantine-color-black-9)",
-        };
+  const { label, bg, color } = getWorkFormat(vacancy);
 
   return (
 
@@ -98,7 +18,7 @@ export const JobCard = ({ vacancy }: Props) => {
 
       <Text className={styles.title}>{vacancy.name}</Text>
         <Group className={styles.wrapper}>
-          <Text className={styles.salary}>{salary}</Text>
+          <Text className={styles.salary}>{formatSalary(vacancy)}</Text>
           <Text className={styles.experience}>
             {formatExperience(vacancy.experience?.name)}
           </Text>
